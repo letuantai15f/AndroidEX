8e5be8c86b90e45f8e0329398dbcf14e5baf7645
letuantai15f <letuantai15f@gmail.com>
2022-04-05 19:29:41 +0700

    tai_19h29_add

--- BaiTapT8/app/src/main/java/com/example/myapplication/MainActivity.java
+++ BaiTapT8/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,36 +2,76 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editText;
     ThongtinSQL thongtinSQL;
     ListView listView;
+    Button btnsave,btndelete;
     ThongTinAdapter thongTinAdapter;
+    TextView txtid;
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         listView=findViewById(R.id.listthongtin);
-        thongtinSQL=new ThongtinSQL(this,"thongtin.sqlite",null,1);
-        thongtinSQL.QueryData("CREATE TABLE IF NOT EXISTS ThongTin(id INTEGER  PRIMARY KEY  AUTOINCREMENT,  ChiTiet VARCHAR(200))");
-        thongtinSQL.QueryData("insert into ThongTin values(1,'ĐA LAT')");
-        Cursor datathongtin=thongtinSQL.GetData("select & from ThongTin");
+        editText=findViewById(R.id.editTextTextPersonName);
+        btnsave=findViewById(R.id.btnsave);
 
+//        ThongtinEntity thongtinEntity=new ThongtinEntity(1,"Da Lat");
         ArrayList<ThongtinEntity> entityArrayList=new ArrayList<>();
+//        entityArrayList.add(thongtinEntity);
         thongTinAdapter=new ThongTinAdapter(this,R.layout.layout_list,entityArrayList);
         listView.setAdapter(thongTinAdapter);
 
+
+
+
+    btnsave.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            String ten=editText.getText().toString();
+            thongtinSQL.QueryData("insert into ThongTin values(null,'"+ten+"')");
+            Cursor datathongtin=thongtinSQL.GetData("select * from ThongTin");
+
+
+
+            while (datathongtin.moveToNext()){
+                String ten2=datathongtin.getString(1);
+                int id=datathongtin.getInt(0);
+                entityArrayList.add(new ThongtinEntity(id,ten2));
+
+            }
+            thongTinAdapter.notifyDataSetChanged();
+
+        }
+    });
+        thongtinSQL=new ThongtinSQL(this,"ThongTin.sqlite",null,1);
+        thongtinSQL.QueryData("CREATE TABLE IF NOT EXISTS ThongTin(stt INTEGER  PRIMARY KEY  AUTOINCREMENT,  ChiTiet VARCHAR(200))");
+//        thongtinSQL.QueryData("insert into ThongTin values(null,'ĐA LAT')");
+        Cursor datathongtin=thongtinSQL.GetData("select * from ThongTin");
+
+
+
         while (datathongtin.moveToNext()){
-            String ten=datathongtin.getString(2);
-                    int id=datathongtin.getInt(1);
+            String ten=datathongtin.getString(1);
+                    int id=datathongtin.getInt(0);
                     entityArrayList.add(new ThongtinEntity(id,ten));
 
         }
+
         thongTinAdapter.notifyDataSetChanged();
 
 
--- BaiTapT8/app/src/main/java/com/example/myapplication/ThongTinAdapter.java
+++ BaiTapT8/app/src/main/java/com/example/myapplication/ThongTinAdapter.java
@@ -1,44 +1,52 @@
 package com.example.myapplication;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class ThongTinAdapter extends ArrayAdapter<ThongtinEntity> {
     private Context context;
     private int resource;
+    ImageButton button;
+    ThongtinSQL thongtinSQL;
     private ArrayList<ThongtinEntity> entityArrayList;
-    public ThongTinAdapter(@NonNull Context context, int resource, @NonNull ArrayList<ThongtinEntity> objects) {
+    public ThongTinAdapter( Context context, int resource,  ArrayList<ThongtinEntity> objects) {
         super(context, resource, objects);
         this.context=context;
         this.resource=resource;
         this.entityArrayList=objects;
     }
 
-    @Override
-    public int getItemViewType(int position) {
-        return super.getItemViewType(position);
-    }
 
     @NonNull
     @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+    public View getView(int position,  View convertView,  ViewGroup parent) {
         convertView= LayoutInflater.from(context).inflate(R.layout.layout_list,parent,false);
-        TextView id=convertView.findViewById(R.id.id);
+        TextView stt=convertView.findViewById(R.id.stt);
         TextView thongtin=convertView.findViewById(R.id.thongtin);
+        button=convertView.findViewById(R.id.btnxoa);
         ThongtinEntity thongtinEntity= entityArrayList.get(position);
         thongtin.setText(thongtinEntity.getThongtin());
-        id.setText(thongtinEntity.getId());
+        stt.setText(Integer.toString(thongtinEntity.getId()) );
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Toast.makeText(context, stt.getText(), Toast.LENGTH_SHORT).show();
+                thongtinSQL.QueryData("Delete From ThongTin where stt="+stt.getText()+"");
+
+            }
+        });
         return  convertView;
     }
 }
