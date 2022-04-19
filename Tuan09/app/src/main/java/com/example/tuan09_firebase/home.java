b0b26705f9a07a0e390c56cbabe139f946150311
letuantai15f <letuantai15f@gmail.com>
2022-04-19 21:19:05 +0700

    firebaselogin_xong

---
+++ Tuan09/app/src/main/java/com/example/tuan09_firebase/home.java
@@ -0,0 +1,14 @@
+package com.example.tuan09_firebase;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class home extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+
+
+    }
+}
--- Tuan09/app/src/main/java/com/example/tuan09_firebase/sign.java
+++ Tuan09/app/src/main/java/com/example/tuan09_firebase/sign.java
@@ -1,41 +1,60 @@
 package com.example.tuan09_firebase;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-import com.google.android.gms.common.api.GoogleApiClient;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class sign extends AppCompatActivity {
     TextView textView;
-    private GoogleApiClient mGoogleApiClient;
+    Button btnlogin;
+    EditText email;
+    EditText pass;
+
+    private static final int REQ_ONE_TAP = 2;  // Can be any integer unique to the Activity.
+    private boolean showOneTapUI = true;
     private static final int RC_SIGN_IN = 0;
+    private FirebaseAuth mauth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.sign_in);
-        textView=findViewById(R.id.txtgg);
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(Plus.API)
-                .addScope(Plus.SCOPE_PLUS_LOGIN)
-                .build();
-        textView.setOnClickListener(new View.OnClickListener() {
+        textView = findViewById(R.id.txtgg);
+        mauth= FirebaseAuth.getInstance();
+        btnlogin=findViewById(R.id.btnSignIn);
+        email=findViewById(R.id.txtemailsign);
+        pass=findViewById(R.id.edtpass);
+        btnlogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+login();
             }
         });
+
     }
-    private void signIn() {
-        Intent signInIntent = googleApiClient.getSignInIntent();
-        startActivityForResult(signInIntent, RC_SIGN_IN);
+    private void login(){
+        String emails,passw;
+        emails=email.getText().toString();
+        passw=pass.getText().toString();
+        mauth.signInWithEmailAndPassword(emails,passw).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Intent intent=new Intent(sign.this,home.class);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
